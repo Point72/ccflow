d282f683f145061b56304c477030025ddff427f8
Samy Rasmy <samy.rasmy@cubistsystematic.com>
2024-09-24 16:35:46 -0400

    lint code

--- ccflow/tests/test_lazy_result.py
+++ ccflow/tests/test_lazy_result.py
@@ -1,6 +1,6 @@
 from typing import ClassVar
 
-from pydantic import model_validator, ConfigDict, PrivateAttr
+from pydantic import ConfigDict, PrivateAttr, model_validator
 
 from ccflow.base import ResultBase, make_lazy_result
 
@@ -8,8 +8,8 @@ from ccflow.base import ResultBase, make_lazy_result
 class MyResult(ResultBase):
     total: ClassVar[int] = 0  # To track instantiations
     value: bool = False
-    model_config = ConfigDict(extra='allow')
-    _private: str = PrivateAttr(default='bar')
+    model_config = ConfigDict(extra="allow")
+    _private: str = PrivateAttr(default="bar")
 
     @model_validator(mode="after")
     def _validate(self):
@@ -30,12 +30,12 @@ def test_make_lazy_result():
     assert MyResult.total == 2  # Accessing the value in the line above did increment the total
     assert lazy_result == MyResult(value=True)
 
-    result = MyResult(value=True, extra_field='foo')
+    result = MyResult(value=True, extra_field="foo")
     assert "value" in result.__pydantic_fields_set__
-    assert result.__pydantic_extra__['extra_field'] == 'foo'
-    assert result.__pydantic_private__['_private'] == 'bar'
+    assert result.__pydantic_extra__["extra_field"] == "foo"
+    assert result.__pydantic_private__["_private"] == "bar"
 
-    lazy_result = make_lazy_result(MyResult, lambda: MyResult(value=True, extra_field='foo'))
+    lazy_result = make_lazy_result(MyResult, lambda: MyResult(value=True, extra_field="foo"))
     assert "value" in lazy_result.__pydantic_fields_set__
-    assert lazy_result.__pydantic_extra__['extra_field'] == 'foo'
-    assert lazy_result.__pydantic_private__['_private'] == 'bar'
+    assert lazy_result.__pydantic_extra__["extra_field"] == "foo"
+    assert lazy_result.__pydantic_private__["_private"] == "bar"
