a2f5e54ee27b014ba9d41b26978257bc53a8ce98
Pascal Tomecek <pascal.tomecek@cubistsystematic.com>
2025-07-01 15:36:47 -0400

    Update tpch queries to better handle decimal type
    Signed-off-by: Pascal Tomecek <pascal.tomecek@cubistsystematic.com>

--- ccflow/examples/tpch/data_generators.py
+++ ccflow/examples/tpch/data_generators.py
@@ -79,20 +79,24 @@ class TPCHDataGenerator(CallableModel):
         self._conn.execute(f"CALL dbgen(sf={self.scale_factor})")
         self._generated = True
 
+    def convert_schema(self, schema: pa.Schema) -> pa.Schema:
+        new_schema = []
+        for field in schema:
+            if pa.types.is_decimal(field.type):
+                new_schema.append(pa.field(field.name, pa.float64()))
+            elif field.type == pa.date32():
+                new_schema.append(pa.field(field.name, pa.timestamp("ns")))
+            else:
+                new_schema.append(field)
+        return pa.schema(new_schema)
+
     @Flow.call
     def __call__(self, context: TPCHTableContext) -> NarwhalsDataFrameResult:
         """Generates data for the TPC-H benchmark."""
         self._generate_if_needed()
         tbl = self._conn.query(f"SELECT * FROM {context.table}")
         tbl_arrow = tbl.to_arrow_table()
-        new_schema = []
-        for field in tbl_arrow.schema:
-            if isinstance(field.type, type(pa.decimal64(1))):
-                new_schema.append(pa.field(field.name, pa.float64()))
-            elif field.type == pa.date32():
-                new_schema.append(pa.field(field.name, pa.timestamp("ns")))
-            else:
-                new_schema.append(field)
-        tbl_arrow = tbl_arrow.cast(pa.schema(new_schema))
+        new_schema = self.convert_schema(tbl_arrow.schema)
+        tbl_arrow = tbl_arrow.cast(new_schema)
         # Convert to Polars DataFrame to use the polars backend by default for downstream calculations (it's faster)
         return NarwhalsDataFrameResult(df=pl.from_arrow(tbl_arrow))
