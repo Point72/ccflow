1d8ea6c965d489027570e3c21b003dca0f1393b3
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-25 09:07:34 -0400

    Bump version: 0.9.0 → 0.9.1
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- ccflow/__init__.py
+++ ccflow/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 # Import exttypes early so modules that import `from ccflow import PyObjectPath` during
 # initialization find it (avoids circular import issues with functions that import utilities
